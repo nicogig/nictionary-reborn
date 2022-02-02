836edc5ccbb2cc5a6c7bdd0a3939f3093aa966f5
Nicola Gigante <nicolagigante@outlook.com>
2022-02-02 12:17:14 +0000

    Initial Commit

---
+++ Nictionary/NictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NictionaryApp.swift
+//  Nictionary
+//
+//  Created by Nicola Gigante on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct NictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
